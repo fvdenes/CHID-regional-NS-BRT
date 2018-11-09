d5f60f544bdeab7fea636c20d3c06bee155b5e5a
Francisco Dénes <fvdenes@gmail.com>
2018-11-09 13:49:31 -0700

    update

--- NS water table preparation.R
+++ NS water table preparation.R
@@ -11,10 +11,14 @@ library(fasterize)
 LCC <- CRS("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
 NNS <- raster("D:/CHID regional NS BRT/NNS.tif") 
 
+disNNS <- disaggregate(NNS,fact=10)
+disNNS
+
 wt1<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_01/wtbl.shp")
 wt1<-spTransform(wt1,LCC)
 wt1_sf<-st_as_sf(wt1)
 wt1r<-fasterize(wt1_sf,NNS,field="Depth")
+wt1rdis<-fasterize(wt1_sf,disNNS,field="Depth")
 rm(wt1)
 plot(wt1r)
 
@@ -22,6 +26,7 @@ wt2<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_02/wtbl.shp")
 wt2<-spTransform(wt2,LCC)
 wt2_sf<-st_as_sf(wt2)
 wt2r<-fasterize(wt2_sf,NNS,field="Depth")
+wt2rdis<-fasterize(wt2_sf,disNNS,field="Depth")
 rm(wt2)
 plot(wt2r)
 
@@ -29,92 +34,98 @@ wt3<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_03/wtbl.shp")
 wt3<-spTransform(wt3,LCC)
 wt3_sf<-st_as_sf(wt3)
 wt3r<-fasterize(wt3_sf,NNS,field="Depth")
+wt3rdis<-fasterize(wt3_sf,disNNS,field="Depth")
 rm(wt3)
 
 wt4<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_04/wtbl.shp")
 wt4<-spTransform(wt4,LCC)
 wt4_sf<-st_as_sf(wt4)
 wt4r<-fasterize(wt4_sf,NNS,field="Depth")
+wt4rdis<-fasterize(wt4_sf,disNNS,field="Depth")
 rm(wt4)
 
 wt5<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_05/wtbl.shp")
 wt5<-spTransform(wt5,LCC)
 wt5_sf<-st_as_sf(wt5)
 wt5r<-fasterize(wt5_sf,NNS,field="Depth")
+wt5rdis<-fasterize(wt5_sf,disNNS,field="Depth")
 rm(wt5)
 
 wt6<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_06/wtbl.shp")
 wt6<-spTransform(wt6,LCC)
 wt6_sf<-st_as_sf(wt6)
 wt6r<-fasterize(wt6_sf,NNS,field="Depth")
+wt6rdis<-fasterize(wt6_sf,disNNS,field="Depth")
 rm(wt6)
 
 wt7<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_07/wtbl.shp")
 wt7<-spTransform(wt7,LCC)
 wt7_sf<-st_as_sf(wt7)
 wt7r<-fasterize(wt7_sf,NNS,field="Depth")
+wt7rdis<-fasterize(wt7_sf,disNNS,field="Depth")
 rm(wt7)
 
 wt8<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_08/wtbl.shp")
 wt8<-spTransform(wt8,LCC)
 wt8_sf<-st_as_sf(wt8)
 wt8r<-fasterize(wt8_sf,NNS,field="Depth")
+wt8rdis<-fasterize(wt8_sf,disNNS,field="Depth")
 rm(wt8)
 
 wt9<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_09/wtbl.shp")
 wt9<-spTransform(wt9,LCC)
 wt9_sf<-st_as_sf(wt9)
 wt9r<-fasterize(wt9_sf,NNS,field="Depth")
+wt9rdis<-fasterize(wt9_sf,disNNS,field="Depth")
 rm(wt9)
 
 wt10<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_10/wtbl.shp")
 wt10<-spTransform(wt10,LCC)
 wt10_sf<-st_as_sf(wt10)
 wt10r<-fasterize(wt10_sf,NNS,field="Depth")
+wt10rdis<-fasterize(wt10_sf,disNNS,field="Depth")
 rm(wt10)
 
 wt11<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_11/wtbl.shp")
 wt11<-spTransform(wt11,LCC)
 wt11_sf<-st_as_sf(wt11)
 wt11r<-fasterize(wt11_sf,NNS,field="Depth")
+wt11rdis<-fasterize(wt11_sf,disNNS,field="Depth")
 rm(wt11)
 
 wt12<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_12/wtbl.shp")
 wt12<-spTransform(wt12,LCC)
 wt12_sf<-st_as_sf(wt12)
 wt12r<-fasterize(wt12_sf,NNS,field="Depth")
+wt12rdis<-fasterize(wt12_sf,disNNS,field="Depth")
 rm(wt12)
 
 wt13<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_13/wtbl.shp")
 wt13<-spTransform(wt13,LCC)
 wt13_sf<-st_as_sf(wt13)
 wt13r<-fasterize(wt13_sf,NNS,field="Depth")
+wt13rdis<-fasterize(wt13_sf,disNNS,field="Depth")
 rm(wt13)
 
 wt14<-readOGR("D:/CHID regional NS BRT/water_table/wtbl_14/wtbl.shp")
 wt14<-spTransform(wt14,LCC)
 wt14_sf<-st_as_sf(wt14)
 wt14r<-fasterize(wt14_sf,NNS,field="Depth")
+wt14rdis<-fasterize(wt14_sf,disNNS,field="Depth")
 rm(wt14)
 
-wtmerge<-mosaic(wt1r,wt2r,wt3r,wt4r,wt5r,wt6r,wt7r,wt8r,wt9r,wt10r,wt11r,wt12r,wt13r,wt14r, fun=mean)
-plot(wtmerge)
+wtmerge<-mosaic(wt1r,wt2r,wt3r,wt4r,wt5r,wt6r,wt7r,wt8r,wt9r,wt10r,wt11r,wt12r,wt13r,wt14r, fun=max)
+wtdismerge<-mosaic(wt1rdis,wt2rdis,wt3rdis,wt4rdis,wt5rdis,wt6rdis,wt7rdis,wt8rdis,wt9rdis,wt10rdis,wt11rdis,wt12rdis,wt13rdis,wt14rdis, fun=max)
 
-summary(getValues(wtmerge))
-
-cc<-which(getValues(wtmerge)==1.5)
-wtmerge@data@values[cc]<-1
-
-nn<-which(getValues(wtmerge)==2.5)
-wtmerge@data@values[nn]<-2
-
-mm<-which(getValues(wtmerge)==3.5)
-wtmerge@data@values[mm]<-3
+unique(wtmerge@data@values)
+unique(getValues(wtdismerge))
 
 plot(wtmerge)
+plot(wtdismerge)
 
-wtmerge_mask<- mask(wtmerge,NNS)
+
 
 plot(wtmerge_mask)
 
 writeRaster(wtmerge_mask,"watertableNNS_LCC",format="GTiff",overwrite=TRUE)
+writeRaster(wtdismerge,"disaggregated_25m_watertableNNS_LCC",format="GTiff",overwrite=TRUE)
--- data preparation.R
+++ data preparation.R
@@ -6,11 +6,19 @@ library(data.table)
 library(rgdal)
 library(dplyr)
 
-load("M:/DataStuff/AvianData/Processed/data_package_2016-04-18.Rdata")	
-load("M:/DataStuff/AvianData/Processed/offsets-v3_2016-04-18.Rdata")
+
+load("D:/Avian data processed/data_package_2016-04-18.Rdata")	
+load("D:/Avian data processed/offsets-v3_2016-04-18.Rdata")
 LCC <- CRS("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
 coordinates(SS) <- c("X", "Y") 
 proj4string(SS) <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+SSLCC <- spTransform(SS, LCC)
+
+offl <- data.table(melt(OFF))
+names(offl) <- c("PKEY","SPECIES","logoffset")
+offl$SPECIES <- as.character(offl$SPECIES)
+offl$PKEY <-as.character(offl$PKEY)
+rm(OFF)
 
 #eco<-shapefile("D:/CHID regional NS BRT/NNS_eco.shp")
 #eco<-rasterize(eco,NNS)
@@ -20,33 +28,27 @@ nalc <- raster("D:/CHID regional Alberta BRT/NA_LandCover_2005_LCC.img")
 NNS <- raster("D:/CHID regional NS BRT/NNS.tif") 
 proj4string(NNS)<-LCC
 
-SSLCC <- spTransform(SS, LCC)
 
-NSSS <- raster::intersect(SSLCC,NNS)
+NNS_shp<-readOGR("D:/CHID regional NS BRT",layer="northernNSmngtunit")
+NNS_shp<-spTransform(NNS_shp,LCC)
+NSSS <- crop(SSLCC,NNS_shp)
 NSSS <- as.data.frame(NSSS)
 
-
-offl <- data.table(melt(OFF))
-names(offl) <- c("PKEY","SPECIES","logoffset")
-offl$SPECIES <- as.character(offl$SPECIES)
-offl$PKEY <-as.character(offl$PKEY)
-rm(OFF)
-
-
-
-
-b2011 <- list.files("M:/DataStuff/SpatialData/Beaudoin/2011/",pattern="tif$")
-setwd("M:/DataStuff/SpatialData/Beaudoin/2011/")
+b2011 <- list.files("D:/Beaudoin/2011/",pattern="tif$")
+setwd("D:/Beaudoin/2011/")
 bs2011 <- stack(raster(b2011[1]))
 for (i in 2:length(b2011)) { bs2011 <- addLayer(bs2011, raster(b2011[i]))}
 names(bs2011) <- gsub("NFI_MODIS250m_2011_kNN_","",names(bs2011))
+
 abs2011 <- crop(bs2011,NNS)
+abs2011<-mask(abs2011,NNS)
 abs2011_1km <- aggregate(abs2011, fact=4, fun=mean)
 r2 <- abs2011_1km[[1]]
 
 ecor1km <- resample(eco, abs2011_1km)
 abs2011_1km <- addLayer(abs2011_1km, ecor1km)
 names(abs2011_1km)[nlayers(abs2011_1km)] <- "eco"
+
 
 wtbl <- raster("D:/CHID regional NS BRT/watertableNNS_LCC.tif")
 wtbl1km<-aggregate(wtbl,fact=4,fun=modal)
@@ -55,20 +57,55 @@ names(abs2011_1km)[nlayers(abs2011_1km)] <- "wtbl"
 
 writeRaster(abs2011_1km,"NS2011rasters",overwrite=TRUE)
 
-b2001 <- list.files("M:/DataStuff/SpatialData/Beaudoin/2001/",pattern="tif$")
-setwd("M:/DataStuff/SpatialData/Beaudoin/2001/")
+roadsNNS<-raster("D:/CHID regional NS BRT/roadsNNS.tif")
+roadsNNS<-crop(roadsNNS,abs2011_1km)
+roadsNNS<-resample(roadsNNS,abs2011_1km)
+roadsNNS<-mask(roadsNNS,abs2011_1km$LandCover_NonVeg_v1)
+abs2011_1km <- addLayer(abs2011_1km, roadsNNS)
+names(abs2011_1km)[nlayers(abs2011_1km)] <- "roads"
+
+climateAW2010 <- list.files("D:/ClimateAdaptWest/baseline19812010/",pattern="asc$")
+setwd("D:/ClimateAdaptWest/baseline19812010/")
+clim2010 <- stack(raster(climateAW2010[1]))
+for (i in 2:length(climateAW2010)) { clim2010 <- addLayer(clim2010, raster(climateAW2010[i]))}
+proj4string(clim2010)<-LCC
+aclim2010 <- crop(clim2010,abs2011_1km)
+aclim2010<-resample(clim2010,abs2011_1km)
+aclim2010<-mask(aclim2010,abs2011_1km$LandCover_NonVeg_v1)
+
+for(i in 1:length(names(aclim2010))){ 
+  abs2011_1km <- addLayer(abs2011_1km, aclim2010[[i]])
+  names(abs2011_1km)[nlayers(abs2011_1km)] <- names(aclim2010[[i]])
+}
+
+
+
+b2001 <- list.files("D:/Beaudoin/2001/",pattern="tif$")
+setwd("D:/Beaudoin/2001/")
 bs2001 <- stack(raster(b2001[1]))
 for (i in 2:length(b2001)) { bs2001 <- addLayer(bs2001, raster(b2001[i]))}
 names(bs2001) <- gsub("NFI_MODIS250m_2001_kNN_","",names(bs2001))
 abs2001 <- crop(bs2001,NNS)
+
 
 dat2011 <- cbind(NSSS, extract(abs2011,as.matrix(cbind(NSSS$X,NSSS$Y))))
 dat2011 <-cbind(dat2011,extract(nalc,as.matrix(cbind(dat2011$X,dat2011$Y)))) 
 names(dat2011)[ncol(dat2011)] <- "LCC"
 dat2011 <-cbind(dat2011,extract(eco,as.matrix(cbind(dat2011$X,dat2011$Y))))
 names(dat2011)[ncol(dat2011)] <- "eco"
-dat2011<-cbind(dat2011,extract(wtbl,as.matrix(cbind(dat2011$X,dat2011$Y))))
+
+disaggregated_wtbl<-raster("D:/CHID regional NS BRT/disaggregated_25m_watertableNNS_LCC.tif")
+dat2011<-cbind(dat2011,extract(disaggregated_wtbl,as.matrix(cbind(dat2011$X,dat2011$Y))))
 names(dat2011)[ncol(dat2011)] <- "wtbl"
+
+dat2011<-cbind(dat2011,extract(roadsNNS,as.matrix(cbind(dat2011$X,dat2011$Y))))
+names(dat2011)[ncol(dat2011)] <- "roads"
+
+for(i in which(names(abs2011_1km)=="AHM"):which(names(abs2011_1km)=="TD") ) {
+  dat2011<-cbind(dat2011,extract(abs2011_1km[[i]],as.matrix(cbind(NSSS$X,NSSS$Y))))
+  names(dat2011)[ncol(dat2011)] <- names(abs2011_1km)[[i]]
+}
+
 
 samprast2011 <- rasterize(cbind(dat2011$X,dat2011$Y), r2, field=1)
 sampsum25 <- focal(samprast2011, w=matrix(1/25, nc=5, nr=5), na.rm=TRUE)
@@ -90,13 +127,21 @@ dat2001 <-cbind(dat2001,extract(nalc,as.matrix(cbind(dat2001$X,dat2001$Y))))
 names(dat2001)[ncol(dat2001)] <- "LCC"
 dat2001 <-cbind(dat2001,extract(eco,as.matrix(cbind(dat2001$X,dat2001$Y))))
 names(dat2001)[ncol(dat2001)] <- "eco"
-dat2001<-cbind(dat2001,extract(wtbl,as.matrix(cbind(dat2001$X,dat2001$Y))))
+
+
+
+dat2001<-cbind(dat2001,extract(disaggregated_wtbl,as.matrix(cbind(dat2001$X,dat2001$Y))))
 names(dat2001)[ncol(dat2001)] <- "wtbl"
+dat2001<-cbind(dat2001,extract(roadsNNS,as.matrix(cbind(dat2001$X,dat2001$Y))))
+names(dat2001)[ncol(dat2001)] <- "roads"
+
+for(i in which(names(abs2011_1km)=="AHM"):which(names(abs2011_1km)=="TD") ) {
+  dat2001<-cbind(dat2001,extract(abs2011_1km[[i]],as.matrix(cbind(NSSS$X,NSSS$Y))))
+  names(dat2001)[ncol(dat2001)] <- names(abs2011_1km)[[i]]
+}
 
 samprast2001 <- rasterize(cbind(dat2001$X,dat2001$Y), r2, field=1)
 sampsum25 <- focal(samprast2001, w=matrix(1/25, nc=5, nr=5), na.rm=TRUE) 
-
-
 dat2001 <- cbind(dat2001,extract(sampsum25,as.matrix(cbind(dat2001$X,dat2001$Y))))
 names(dat2001)[ncol(dat2001)] <- "sampsum25"
 dat2001$wt <- 1/dat2001$sampsum25
@@ -107,7 +152,8 @@ dat2001$SS <- as.character(dat2001$SS)
 dat2001$PCODE <- as.character(dat2001$PCODE)
 write.csv(dat2001,"NNSdat2001.csv")
 
-PC <- inner_join(PCTBL,PKEY[,1:8],by=c("PKEY","SS","PCODE"))
+PC <- inner_join(PCTBL,PKEY[,1:8],by=c("PKEY","SS"))[,-1]
+colnames(PC)[10]<-"PCODE"
 PC <- inner_join(PC,SS@data[,c(2,5)],by="SS")
 
 
@@ -117,10 +163,10 @@ NSPC$SS <- as.character(NSPC$SS)
 NSPC$PKEY <- as.character(NSPC$PKEY)
 NSPC$PCODE <- as.character(NSPC$PCODE)
 NSPC$SPECIES <- as.character(NSPC$SPECIES)
-NSPC2001 <- NSPC[NSPC$YEAR < 2006,] #n=
-NSPC2011 <- NSPC[NSPC$YEAR > 2005,] #n=
-survey2001 <- aggregate(NSPC2001$ABUND, by=list("PKEY"=NSPC2001$PKEY,"SS"=NSPC2001$SS,"PCODE"=NSPC2001$PCODE), FUN=sum) #n=
-survey2011 <- aggregate(NSPC2011$ABUND, by=list("PKEY"=NSPC2011$PKEY,"SS"=NSPC2011$SS,"PCODE"=NSPC2011$PCODE), FUN=sum) #n=
+NSPC2001 <- NSPC[NSPC$YEAR < 2006,] #n=83570
+NSPC2011 <- NSPC[NSPC$YEAR > 2005,] #n=91656
+survey2001 <- aggregate(NSPC2001$ABUND, by=list("PKEY"=NSPC2001$PKEY,"SS"=NSPC2001$SS,"PCODE"=NSPC2001$PCODE), FUN=sum) #n=9865
+survey2011 <- aggregate(NSPC2011$ABUND, by=list("PKEY"=NSPC2011$PKEY,"SS"=NSPC2011$SS,"PCODE"=NSPC2011$PCODE), FUN=sum) #n=10505
 
 speclist<-levels(as.factor(offl$SPECIES))
 
